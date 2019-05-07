8c0607e1c8a329af3c5e0b3bc97affcf8c77d4af
qiaoyiyang252 <qiaoyiyang252@hyjy.com>
2019-05-07 18:14:39 +0800

    切换开发机器备份

--- lib/Caller.php
+++ lib/Caller.php
@@ -1,29 +1,82 @@
 <?php
 class Caller
 {
-
-    public function get($app, string $path, string...$headers)
+    /**
+     * GET方式调用其他Service
+     *
+     * @param $app app名字
+     * @param $path 路由
+     * @param $headers header头
+     */
+    public function get($app, string $path, array $headers)
     {
+        return $this->_do('GET', $app, $path, [], $headers);
 
     }
-    public function post($app, string $path, array $data, string...$headers)
+
+    /**
+     * POST方式调用其他Service
+     *
+     * @param $app app名字
+     * @param $path 路由
+     * @param $data 发送数据
+     * @param $headers header头
+     */
+    public function post($app, string $path, array $data, array $headers)
     {
+        return $this->_do('POST', $app, $path, $data, $headers);
 
     }
+
+    /**
+     * PUT方式调用其他Service
+     *
+     * @param $app app名字
+     * @param $path 路由
+     * @param $data 发送数据
+     * @param $headers header头
+     */
     public function put($app, string $path, array $data, string...$headers)
     {
-
+        return $this->_do('PUT', $app, $path, $data, $headers);
     }
+
+    /**
+     * delete方式调用其他Service
+     *
+     * @param $app app名字
+     * @param $path 路由
+     * @param $data 发送数据
+     * @param $headers header头
+     */
     public function delete($app, string $path, array $data, string...$headers)
     {
-
+        return $this->_do('DELETE', $app, $path, $data, $headers);
     }
+
+    /**
+     * header方式调用其他Service
+     *
+     * @param $app app名字
+     * @param $path 路由
+     * @param $data 发送数据
+     * @param $headers header头
+     */
     public function head($app, string $path, array $data, string...$headers)
     {
-
+        return $this->_do('HEAD', $app, $path, $data, $headers);
     }
-    function do($method, $app, string $path, array $data, string...$headers) {
 
+    /**
+     * 负载均衡调度Service
+     *
+     * @param $app app名字
+     * @param $path 路由
+     * @param $data 发送数据
+     * @param $headers header头
+     */
+    private function _do($method, $app, string $path, array $data, $headers)
+    {
     }
 
 }
--- lib/Discover.php
+++ lib/Discover.php
@@ -7,6 +7,15 @@ class Discover
 {
     private $config;
     private $redis;
+    /**
+     * 服务发现存储服务信息
+     * {
+     *     "addr":"10.1.1.1",
+     *     "port":"",
+     *     "weight":""
+     * }
+     */
+    private $appNodes;
 
     public function __construct($service_config = __DIR__ . '/../service_config.php')
     {
@@ -14,15 +23,9 @@ class Discover
         if (!$this->config) {
             throw new \Exception('无法加载 ' . $service_config);
         }
-        //连接服务注册中心的 Redis 服务
-        $this->redis = new Redis();
-        $this->redis->connect($this->config['redis']['address'], $this->config['redis']['port']);
-
-        //授权
-        $this->redis->auth($this->config['redis']['password']);
-
-        $this->redis->select($this->config['redis']['database']);
+        $this->_checkRedis();
     }
+
     /**
      * Load configs.
      *
@@ -37,6 +40,34 @@ class Discover
         require_once $configFile;
         $this->config = $configs;
     }
+
+    /**
+     * 服务发现
+     *
+     * @param $appName service集群名字
+     */
+    public function fetchApp($appName)
+    {
+        $this->_checkRedis();
+
+        $appNodesResults = $this->redis->hGetall($appName);
+        foreach ($this->appNodes[$appName] as $key => $val) {
+            if (empty($appNodesResults[$val['addr']])) {
+                //logInfo("remove node", "node", node, "nodes", appNodes[app])
+                //通知所有节点，该节点下线
+                $this->pushNode($appName, $val['addr'], 0);
+            }
+        }
+
+        foreach ($appNodesResults as $key => $val) {
+            $weight = $val;
+            //logInfo("update node", "nodes", appNodes[app])
+            //通知所有其他节点，该服务节点更新
+            $this->pushNode($appName, $val['addr'], $weight);
+
+        }
+    }
+
     /**
      * 注册服务
      *
@@ -47,11 +78,12 @@ class Discover
      */
     public function register(string $serviceIp, int $port)
     {
+        $this->_checkRedis();
 
         //在服务注册中心的redis中注册服务
         $this->redis->hSet($this->config['service']['appName'], $serviceIp . ':' . $port, $this->config['service']['weight']);
 
-        $this->push($this->config['service']['appName'], $serviceIp, $this->config['service']['weight']);
+        $this->pushNode($this->config['service']['appName'], $serviceIp, $this->config['service']['weight']);
 
         // echo "Connection to server successfully";
         // //查看服务是否运行
@@ -66,6 +98,8 @@ class Discover
      */
     public function destroy(string $serviceIp, int $port)
     {
+        $this->_checkRedis();
+
         $this->redis->hDel($this->config['service']['appName'], $serviceIp . ':' . $port);
 
     }
@@ -77,11 +111,39 @@ class Discover
      * @param $addr
      * @param $weight
      */
-    private function push($appName, $addr, $weight)
+    private function pushNode($appName, $addr, $weight)
     {
         //serverRedisPool.Do("PUBLISH", config.RegistryPrefix+"CH_"+config.App, fmt.Sprintf("%s %d", addr, config.Weight))
-
+        $this->_checkRedis();
         $this->redis->publish($this->config['service']['registryPrefix'] . 'CH_' . $appName, $addr . ' ' . $weight);
+    }
+
+    /**
+     * 检查redis连接状态是否完好，如果已经失效，则重新连接
+     */
+    private function _checkRedis()
+    {
+
+        //重新连接时，释放上一次的实例
+        if (!empty($this->redis)) {
+            //测试当前连接是否有效，有效直接返回
+            if ($this->redis->ping() == 'PONG') {
+                return;
+            }
+            unset($this->redis);
+        }
+
+        //连接服务注册中心的 Redis 服务
+        $this->redis = new Redis();
+        $this->redis->connect($this->config['redis']['address'], $this->config['redis']['port']);
+
+        //授权
+        if (!empty($this->config['redis']['password'])) {
+            $this->redis->auth($this->config['redis']['password']);
+        }
+
+        $this->redis->select($this->config['redis']['database']);
+        return;
     }
 
 }
